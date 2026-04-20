9d65adee116257a84fbd6fc2f0ab3012c7b1dcf7
KollaNagaManasa <2024tm93519@wilp.bits-pilani.ac.in>
2026-04-20 22:10:09 +0530

    Update Aceest_fitness.py

--- app/Aceest_fitness.py
+++ app/Aceest_fitness.py
@@ -1,1 +1,28 @@
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+# Home route
+@app.route('/')
+def home():
+    return jsonify({
+        "message": "Welcome to ACEest Fitness Gym App",
+        "version": "v1"
+    })
+
+# Members route
+@app.route('/members')
+def members():
+    return jsonify({
+        "members": ["John", "Alice", "Rahul"]
+    })
+
+# Plans route
+@app.route('/plans')
+def plans():
+    return jsonify({
+        "plans": ["Basic", "Premium", "Pro"]
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
